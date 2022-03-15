6b1fb05d58a158743bb585ebef809f443306fe36
Bazabizi <99665336+Bazabizi@users.noreply.github.com>
2022-03-15 22:18:35 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
+ * @filedes - file descriptor
+ * @buf - pointer to array of character
+ * @nbyte - number of bytes to print
  * Return: Always 0 (Success)
  */
 int write(int filedes, const char *buf, unsigned int nbyte);
